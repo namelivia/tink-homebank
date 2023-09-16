f61c7fa36ca81fbc28d1f85aaf2512e73c84f899
José Ignacio Amelivia Santiago <nacho.amelivia@travelperk.com>
2023-09-16 20:20:49 +0200

    Insert categories

--- cat/categories.py
+++ cat/categories.py
@@ -2,6 +2,15 @@ import sqlite3
 
 database_file = 'categories.db'
 
+def insert_item(category_name, description):
+    category_id = _get_id_by_name(category_name)
+    conn = sqlite3.connect(database_file)
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO items (category_id, name) VALUES (?, ?)", (category_id, description))
+    conn.commit()
+    cursor.close()
+    conn.close()
+
 def get_category(description):
     conn = sqlite3.connect(database_file)
     cursor = conn.cursor()
@@ -27,6 +36,17 @@ def _get_category_name_by_id(id):
     conn = sqlite3.connect(database_file)
     cursor = conn.cursor()
     cursor.execute("SELECT name FROM categories WHERE id = ?", (id,))
+    data = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    if len(data) == 0:
+        return None
+    return data[0][0]
+
+def _get_id_by_name(name):
+    conn = sqlite3.connect(database_file)
+    cursor = conn.cursor()
+    cursor.execute("SELECT id FROM categories WHERE name = ?", (name,))
     data = cursor.fetchall()
     cursor.close()
     conn.close()
