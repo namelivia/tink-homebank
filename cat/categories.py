b46881f354f281991cfbbcb9918ea32c6bd1cc16
José Ignacio Amelivia Santiago <nacho.amelivia@travelperk.com>
2023-09-16 20:02:02 +0200

    Replace categories CSV for SQLite

--- cat/categories.py
+++ cat/categories.py
@@ -1,18 +1,35 @@
-import csv
+import sqlite3
 
-# Quick and dirty category manager
-
+database_file = 'categories.db'
 
 def get_category(description):
-    with open('categories') as csvfile:
-        spamreader = csv.reader(csvfile, delimiter=',')
-        for row in spamreader:
-            if description in row[1:]:
-                return row[0]
+    conn = sqlite3.connect(database_file)
+    cursor = conn.cursor()
+    cursor.execute(f"SELECT category_id FROM items WHERE name = '{description}'")
+    data = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    if len(data) == 0:
         return None
-
+    category_id = data[0][0]
+    return _get_category_name_by_id(category_id)
 
 def get_all_categories():
-    with open('categories') as csvfile:
-        spamreader = csv.reader(csvfile, delimiter=',')
-        return [row[0] for row in spamreader]
+    conn = sqlite3.connect(database_file)
+    cursor = conn.cursor()
+    cursor.execute("SELECT name FROM categories")
+    data = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return [entry[0] for entry in data]
+
+def _get_category_name_by_id(id):
+    conn = sqlite3.connect(database_file)
+    cursor = conn.cursor()
+    cursor.execute(f"SELECT name FROM categories WHERE id = {id}")
+    data = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    if len(data) == 0:
+        return None
+    return data[0][0]
